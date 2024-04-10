9af3b31396617ce706c4684fc596eb8772d100a3
Joseph-Chege <mjchege23@gmail.com>
2024-04-10 09:47:51 +0300

    Created functions and used the filter() method to check through the arrays

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(drivers, name){
+
+    const searchName = name.toLowerCase();
+
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === searchName);
+
+    return matchingDrivers
+
+}
+
+function fuzzyMatch (drivers, query){
+    return drivers.filter(driver => driver.startsWith(query));
+
+}
+
+function matchName(drivers, string){
+    return drivers.filter(driver => driver.name === string)
+
+}
